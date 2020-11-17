7b319fd7851486b489ed3868c26ed62cf6ac210d
James Stevenson <james.m.stevenson@gmail.com>
2020-11-16 22:06:02 -0500

    Add pg settings for Herku, upgrade Python runtime

--- cheeseshop/settings.py
+++ cheeseshop/settings.py
@@ -74,13 +74,16 @@ WSGI_APPLICATION = 'cheeseshop.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get("DATABASE_URL"):
+    db_from_env = django_heroku.dj_database_url.config(conn_max_age=600)
+    DATABASES = {'default': db_from_env}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 # Password validation
